ed5a05bd23c7b6326cd0f3bb70fad0407e97316b
kylanhurt <kylan.hurt@gmail.com>
2020-03-22 16:09:19 -0500

    Move from LESs to SCSS

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -45,7 +45,7 @@ const imageInlineSizeLimit = parseInt(
 const useTypeScript = fs.existsSync(paths.appTsConfig);
 
 // style files regexes
-const cssRegex = /\.(?:le|c)ss$/;
+const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
@@ -82,12 +82,6 @@ module.exports = function(webpackEnv) {
       {
         loader: require.resolve('css-loader'),
         options: cssOptions,
-      },
-      {
-        loader: require.resolve('less-loader'),
-        options: {
-          importLoaders: 1,
-        },
       },
       {
         // Options for PostCSS as we reference these options twice
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles/App.less'
+import './styles/App.scss'
 import {
   BrowserRouter as Router,
   Switch,
